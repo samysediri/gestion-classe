4f87a57c400d18adc2ebc97a3747ec013eb6e0a9
samysediri <samysediri65@gmail.com>
2026-04-02 18:18:18 -0400

    Update page.tsx

--- app/ecran/page.tsx
+++ app/ecran/page.tsx
@@ -21,50 +21,63 @@ export default function Ecran(){
     return ()=> clearInterval(interval)
   },[])
 
+  // logique cumulative (IMPORTANT)
   const manquement = eleves.filter(e => e.niveau >= 1)
   const retenue = eleves.filter(e => e.niveau >= 2)
   const retrait = eleves.filter(e => e.niveau >= 3)
 
   return(
 
-    <div className="w-screen h-screen flex flex-col">
+    <div className="w-screen h-screen flex flex-col bg-gray-100">
 
-      {/* titre */}
-      <div className="text-center text-4xl font-bold py-6">
+      {/* TITRE */}
+      <div className="text-center text-6xl font-bold py-8">
         Gestion de classe
       </div>
 
-      {/* colonnes */}
-      <div className="flex flex-1 text-center">
+      {/* COLONNES */}
+      <div className="flex flex-1">
 
         {/* MANQUEMENT */}
-        <div className="flex-1 bg-yellow-300 p-6">
-          <h2 className="text-3xl font-bold mb-6">Manquement</h2>
+        <div className="flex-1 bg-yellow-300 flex flex-col items-center pt-8">
+          <div className="text-5xl font-bold mb-6">
+            Manquement
+          </div>
+
+          <div className="w-full border-t-2 border-black mb-8"></div>
 
           {manquement.map(e=>(
-            <div key={e.id} className="text-3xl mb-2">
+            <div key={e.id} className="text-7xl mb-8 font-semibold">
               {e.nom} #{e.regle_manquement}
             </div>
           ))}
         </div>
 
         {/* RETENUE */}
-        <div className="flex-1 bg-orange-300 p-6">
-          <h2 className="text-3xl font-bold mb-6">Retenue</h2>
+        <div className="flex-1 bg-orange-300 flex flex-col items-center pt-8">
+          <div className="text-5xl font-bold mb-6">
+            Retenue
+          </div>
+
+          <div className="w-full border-t-2 border-black mb-8"></div>
 
           {retenue.map(e=>(
-            <div key={e.id} className="text-3xl mb-2">
+            <div key={e.id} className="text-7xl mb-8 font-semibold">
               {e.nom} #{e.regle_retenue}
             </div>
           ))}
         </div>
 
         {/* RETRAIT */}
-        <div className="flex-1 bg-red-300 p-6">
-          <h2 className="text-3xl font-bold mb-6">Retrait</h2>
+        <div className="flex-1 bg-red-300 flex flex-col items-center pt-8">
+          <div className="text-5xl font-bold mb-6">
+            Retrait
+          </div>
+
+          <div className="w-full border-t-2 border-black mb-8"></div>
 
           {retrait.map(e=>(
-            <div key={e.id} className="text-3xl mb-2">
+            <div key={e.id} className="text-7xl mb-8 font-semibold">
               {e.nom} #{e.regle_retrait}
             </div>
           ))}
