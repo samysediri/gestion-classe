63443f5ea06e1fea8dd233c3cbf2f66bfb842c35
samysediri <samysediri65@gmail.com>
2026-04-04 01:25:11 -0400

    Update page.tsx

--- app/telecommande/[id]/page.tsx
+++ app/telecommande/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useMemo, useRef, useState } from "react"
-import { supabase } from "../../lib/supabase"
+import { supabase } from "../../../lib/supabase"
 import { useParams } from "next/navigation"
 
 type Eleve = {
